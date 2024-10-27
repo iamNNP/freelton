6508ec6383ea362891c9163266f5506728ce663b
iamNNP <alexander.markovskiy@mail.ru>
2024-10-28 01:30:16 +0300

    Url change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,10 @@ export const Header = () => {
     <header>
       <div className="wrapper site-header__wrapper">
         <nav className="nav">
-          <a href="/freelton/place">Place</a>
-          <a href="/freelton/take">Take</a>
-          <a href="/freelton/learn">Learn</a>
-          <a href="/freelton/profile">Profile</a>
+          <a href="#/freelton/place">Place</a>
+          <a href="#/freelton/take">Take</a>
+          <a href="#/freelton/learn">Learn</a>
+          <a href="#/freelton/profile">Profile</a>
         </nav>
         <TonConnectButton />
       </div>
