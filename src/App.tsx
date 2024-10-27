16da56b205fb08bbe39974b168c7464400f1d9c6
iamNNP <alexander.markovskiy@mail.ru>
2024-10-28 01:04:55 +0300

    Url change

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/freelton/" element={<Home />} />
-      <Route path="/freelton/learn" element={<Learn />} />
+      <Route path="/freelton/learn/" element={<Learn />} />
     </Routes>
   );
 }
