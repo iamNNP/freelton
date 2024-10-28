65374ab62d8cb53736a62cf6395d8a9f77ef30d5
iamNNP <alexander.markovskiy@mail.ru>
2024-10-28 22:49:51 +0300

    Add profile page style

--- src/components/UserData.tsx
+++ src/components/UserData.tsx
@@ -6,40 +6,40 @@ export const UserData = () => {
     <div className='user__data'>
         {user ? (
           <div>
-            <h3>
+            <h3 className="user__data_element">
               Avatar
             </h3>
-            <div>
+            <div className="user__data_element">
               <img src={ user.photo_url } alt="User avatar" />
             </div>
-            <h3>
+            <h3 className="user__data_element">
               Telegram Id
             </h3>
-            <div>
+            <div className="user__data_element">
               { user.id }
             </div>
-            <h3>
+            <h3 className="user__data_element">
               First name
             </h3>
-            <div>
+            <div className="user__data_element">
               { user.first_name }
             </div>
-            <h3>
+            <h3 className="user__data_element">
               Last name
             </h3>
-            <div>
+            <div className="user__data_element">
               { user.last_name }
             </div>
-            <h3>
+            <h3 className="user__data_element">
               Username
             </h3>
-            <div>
+            <div className="user__data_element">
               @{ user.username }
             </div>
-            <h3>
+            <h3 className="user__data_element">
               Language code
             </h3>
-            <div>
+            <div className="user__data_element">
               { user.language_code }
             </div>
           </div>
