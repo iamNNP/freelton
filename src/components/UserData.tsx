9aeab25e35c9baab0c8eb28a8727d54c3252c088
iamNNP <alexander.markovskiy@mail.ru>
2024-10-28 21:46:24 +0300

    Profile page min styles

--- src/components/UserData.tsx
+++ src/components/UserData.tsx
@@ -3,14 +3,43 @@ import { useTelegram } from '../hooks/useTelegram';
 export const UserData = () => {
   const { user } = useTelegram();
   return (
-    <div>
+    <div className='user_data'>
         {user ? (
           <div>
-            <div>{ user.id }</div>
-            <div>{ user.first_name }</div>
-            <div>{ user.last_name }</div>
-            <div>{ user.username }</div>
-            <div>{ user.language_code }</div>
+            <h3>
+              Telegram Id
+            </h3>
+            <div>
+              <b>
+                { user.id }
+              </b>
+            </div>
+            <h3>
+              First name
+            </h3>
+            <div>
+              <b>
+                { user.first_name }
+              </b>
+            </div>
+            <h3>
+              Last name
+            </h3>
+            <div>
+              { user.last_name }
+            </div>
+            <h3>
+              Username
+            </h3>
+            <div>
+              @{ user.username }
+            </div>
+            <h3>
+              Language code
+            </h3>
+            <div>
+              { user.language_code }
+            </div>
           </div>
         ) : (
           <div>Unable to get user data</div>
