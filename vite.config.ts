bbf8dc19cdbb83eab48d47f9876ced46b2060357
iamNNP <alexander.markovskiy@mail.ru>
2024-10-28 17:21:37 +0300

    Gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  base: '/freelton/',
+  base: './',
+  plugins: [react(), nodePolyfills()]
 });
