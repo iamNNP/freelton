f5dfde06c32ab8d1f564178d5a1d4cb244d51800
iamNNP <alexander.markovskiy@mail.ru>
2024-10-28 21:19:03 +0300

    Minor change

--- src/components/UserData.tsx
+++ src/components/UserData.tsx
@@ -1,17 +1,16 @@
 import { useTelegram } from '../hooks/useTelegram';
 
 export const UserData = () => {
-    const { user } = useTelegram();
+  const { user } = useTelegram();
   return (
     <div>
         {user ? (
           <div>
-            <div>logo</div>
-            <div>user.id</div>
-            <div>user.first_name</div>
-            <div>user.last_name</div>
-            <div>user.username</div>
-            <div>user.language_code</div>
+            <div>{ user.id }</div>
+            <div>{ user.first_name }</div>
+            <div>{ user.last_name }</div>
+            <div>{ user.username }</div>
+            <div>{ user.language_code }</div>
           </div>
         ) : (
           <div>Unable to get user data</div>
