448a9e5cd0b1327fbffe57afe84df3546feb98c2
iamNNP <alexander.markovskiy@mail.ru>
2024-11-10 14:47:37 +0300

    Add TelegramUI styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { BrowserRouter } from 'react-router-dom';
+import '@telegram-apps/telegram-ui/dist/styles.css';
+import { AppRoot } from '@telegram-apps/telegram-ui';
 
 const manifestUrl = 'https://iamnnp.github.io/freelton/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <BrowserRouter>
-      <App />
+      <AppRoot>
+        <App />
+      </AppRoot>,
     </BrowserRouter>
   </TonConnectUIProvider>,
 )
