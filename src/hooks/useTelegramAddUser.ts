e06a5629e4b740518f1ee101c066fb0d1164dec8
iamNNP <alexander.markovskiy@mail.ru>
2024-11-18 21:15:20 +0300

    Change adding user

--- src/hooks/useTelegramAddUser.ts
+++ src/hooks/useTelegramAddUser.ts
@@ -9,8 +9,12 @@ export const useTelegramAddUser = (user: User | any) => {
     if (user && user.telegramUserId) {
       (async () => {
         console.log(user, `https://freelton.org/api/add-user`, `https://freelton.org/api/save-photo`);
-        await axios.post(`https://freelton.org/api/add-user`, user)
-        .then(() => console.log(''))
+        await axios.post(`https://freelton.org/api/add-user`, user, {
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        })
+        .then(() => console.log('User added successfully'))
         .catch((error) => console.error('Error while adding user: ', error));
         // await axios.post(`https://freelton.org/api/save-photo`, user.photoUrl)
         //   .then(() => console.log(''))
